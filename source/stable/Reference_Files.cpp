d79850b0f8b6a0607719837f59be08689739469c
ChaoticBrave <90638442+ChaoticBrave@users.noreply.github.com>
2023-01-30 19:21:48 +0100

    Update Reference_Files.cpp

--- source/stable/Reference_Files.cpp
+++ source/stable/Reference_Files.cpp
@@ -13,6 +13,8 @@ Reference_Files::Reference_Files() {
 void Reference_Files::loadFiles() {
 	com = "8376858345484948";
 	zones = { "ZONE009.ZND", "ZONE011.ZND", "ZONE012.ZND", "ZONE013.ZND", "ZONE014.ZND", "ZONE015.ZND", "ZONE017.ZND", "ZONE022.ZND", "ZONE024.ZND", "ZONE028.ZND", "ZONE030.ZND", "ZONE032.ZND", "ZONE040.ZND", "ZONE048.ZND", "ZONE049.ZND", "ZONE050.ZND", "ZONE051.ZND", "ZONE052.ZND", "ZONE053.ZND", "ZONE055.ZND", "ZONE056.ZND" };
+	r_zones = { 9, 11, 12, 13, 14, 15, 16, 17, 22, 23, 24, 25, 28, 29, 30, 31, 32, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56 };
+	zones_for_rooms = { "ZONE009.ZND", "ZONE011.ZND", "ZONE012.ZND", "ZONE013.ZND", "ZONE014.ZND", "ZONE015.ZND", "ZONE016.ZND", "ZONE017.ZND", "ZONE022.ZND", "ZONE023.ZND", "ZONE024.ZND", "ZONE025.ZND", "ZONE028.ZND", "ZONE029.ZND", "ZONE030.ZND", "ZONE031.ZND", "ZONE032.ZND", "ZONE034.ZND", "ZONE035.ZND", "ZONE036.ZND", "ZONE037.ZND", "ZONE038.ZND", "ZONE039.ZND", "ZONE040.ZND", "ZONE041.ZND", "ZONE042.ZND", "ZONE043.ZND", "ZONE044.ZND", "ZONE045.ZND", "ZONE046.ZND", "ZONE047.ZND", "ZONE048.ZND", "ZONE049.ZND", "ZONE050.ZND", "ZONE051.ZND", "ZONE052.ZND", "ZONE053.ZND", "ZONE054.ZND", "ZONE055.ZND", "ZONE056.ZND" };
 	map_sum = { 19, 1, 2, 18, 1, 13, 1, 1, 10, 6, 16, 5, 19, 7, 6, 2, 24, 1, 1, 1, 1, 1, 1, 26, 3, 1, 1, 1, 1, 1, 1, 27, 13, 24, 29, 8, 32, 5, 23, 26 };
 	l1 = { "MAP009.MPD", "MAP010.MPD", "MAP011.MPD", "MAP012.MPD", "MAP013.MPD", "MAP014.MPD", "MAP015.MPD", "MAP016.MPD", "MAP018.MPD", "MAP019.MPD", "MAP020.MPD", "MAP021.MPD", "MAP022.MPD", "MAP024.MPD", "MAP412.MPD" };
 	l2 = { "MAP025.MPD" };
@@ -36,9 +38,15 @@ void Reference_Files::loadFiles() {
 	l20 = { "MAP360.MPD", "MAP362.MPD", "MAP365.MPD", "MAP366.MPD", "MAP367.MPD", "MAP368.MPD", "MAP369.MPD", "MAP371.MPD", "MAP373.MPD", "MAP375.MPD", "MAP377.MPD", "MAP378.MPD", "MAP379.MPD", "MAP380.MPD", "MAP381.MPD" };
 	l21 = { "MAP382.MPD", "MAP383.MPD", "MAP384.MPD", "MAP385.MPD", "MAP386.MPD", "MAP387.MPD", "MAP388.MPD", "MAP389.MPD", "MAP390.MPD", "MAP391.MPD", "MAP392.MPD", "MAP394.MPD", "MAP395.MPD", "MAP396.MPD", "MAP397.MPD", "MAP398.MPD", "MAP399.MPD", "MAP402.MPD", "MAP403.MPD", "MAP404.MPD", "MAP405.MPD", "MAP407.MPD" };
 	map_list = { l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l12, l13, l14, l15, l16, l17, l18, l19, l20, l21 };
+	warp_maps = { "MAP025.MPD", "MAP283.MPD", "MAP226.MPD", "MAP112.MPD", "MAP239.MPD", "MAP166.MPD", "MAP224.MPD", "MAP342.MPD", "MAP171.MPD" };
+	warps = { 56540, 47384, 50644, 49588, 50656, 54328, 55092, 50792, 53976 };
+	zoc = { 13, 32, 40, 29, 51, 49, 53, 30, 24 };
+	moc = { 0, 5, 21, 2, 3, 0, 0, 0, 8 };
+	coc = { "680008", "690044", "130044", "980016", "670008", "030052", "030052", "680008", "420004" };
 	has_chest = { "MAP018.MPD", "MAP027.MPD", "MAP409.MPD", "MAP025.MPD", "MAP026.MPD", "MAP408.MPD", "MAP032.MPD", "MAP033.MPD", "MAP037.MPD", "MAP045.MPD", "MAP050.MPD", "MAP079.MPD", "MAP081.MPD", "MAP095.MPD", "MAP124.MPD", "MAP142.MPD", "MAP175.MPD", "MAP176.MPD", "MAP177.MPD", "MAP202.MPD", "MAP203.MPD", "MAP350.MPD", "MAP227.MPD", "MAP241.MPD", "MAP240.MPD", "MAP254.MPD", "MAP255.MPD", "MAP259.MPD", "MAP261.MPD", "MAP270.MPD", "MAP276.MPD", "MAP289.MPD", "MAP292.MPD", "MAP295.MPD", "MAP306.MPD", "MAP352.MPD", "MAP358.MPD", "MAP326.MPD", "MAP331.MPD", "MAP333.MPD", "MAP336.MPD", "MAP345.MPD", "MAP347.MPD", "MAP381.MPD", "MAP380.MPD", "MAP379.MPD", "MAP378.MPD", "MAP397.MPD", "MAP399.MPD", "MAP405.MPD", "MAP407.MPD" };
 	has_key_or_sig = { "MAP270.MPD", "MAP254.MPD", "MAP255.MPD", "MAP227.MPD", "MAP259.MPD", "MAP142.MPD", "MAP241.MPD", "MAP345.MPD" };
 	has_boss = { "MAP025.MPD", "MAP026.MPD", "MAP060.MPD", "MAP061.MPD", "MAP150.MPD", "MAP266.MPD", "MAP277.MPD", "MAP282.MPD", "MAP223.MPD", "MAP201.MPD", "MAP203.MPD", "MAP124.MPD", "MAP286.MPD", "MAP298.MPD", "MAP290.MPD", "MAP248.MPD", "MAP249.MPD", "MAP252.MPD", "MAP366.MPD", "MAP371.MPD", "MAP235.MPD", "MAP312.MPD", "MAP257.MPD", "MAP334.MPD", "MAP340.MPD", "MAP140.MPD", "MAP142.MPD", "MAP145.MPD", "MAP069.MPD", "MAP067.MPD", "MAP063.MPD", "MAP079.MPD", "MAP090.MPD", "MAP083.MPD", "MAP094.MPD", "MAP096.MPD", "MAP350.MPD", "MAP344.MPD", "MAP377.MPD", "MAP383.MPD", "MAP387.MPD", "MAP392.MPD", "MAP396.MPD", "MAP404.MPD" };
+	crashRooms = { "MAP052.MPD", "MAP115.MPD"  };
 }
 
 
@@ -54,22 +62,6 @@ vector<vector<string>> Reference_Files::getMapList() {
 	return map_list;
 }
 
-vector<vector<string>> Reference_Files::getIdList() {
-	return map_id;
-}
-
-list<string> Reference_Files::getZones() {
-	return zones;
-}
-
-list<string> Reference_Files::getKSCheck() {
-	return has_key_or_sig;
-}
-
-vector<int> Reference_Files::getMapSum() {
-	return map_sum;
-}
-
 vector<string> Reference_Files::getIndivZone(vector<string> aZoneList) {
 	if (std::find(map_list.begin(), map_list.end(), aZoneList) != map_list.end()) {
 		return aZoneList;
@@ -81,6 +73,22 @@ vector<string> Reference_Files::getIndivZone(vector<string> aZoneList) {
 
 }
 
+list<string> Reference_Files::getZones() {
+	return zones;
+}
+
+list<string> Reference_Files::getZonesFR() {
+	return zones_for_rooms;
+}
+
+list<string> Reference_Files::getKSCheck() {
+	return has_key_or_sig;
+}
+
+vector<int> Reference_Files::getMapSum() {
+	return map_sum;
+}
+
 vector<string> Reference_Files::getChestCheck() {
 	return has_chest;
 }
@@ -89,3 +97,42 @@ vector<string> Reference_Files::getBossRooms() {
 	return has_boss;
 }
 
+vector<int> Reference_Files::getRawZones() {
+	return r_zones;
+}
+
+vector<string> Reference_Files::getWarpMaps() {
+	return warp_maps;
+}
+
+vector<int> Reference_Files::getWarps() {
+	return warps;
+}
+
+vector<int> Reference_Files::getZonesOfChoice() {
+	return zoc;
+}
+
+vector<int> Reference_Files::getMapsOfChoice() {
+	return moc;
+}
+
+vector<string> Reference_Files::getCoordinatesOfChoice() {
+	return coc;
+}
+
+void Reference_Files::delZone(int anIndex) {
+	zoc.erase(std::next(zoc.begin(), anIndex));
+}
+
+void Reference_Files::delMap(int anIndex) {
+	moc.erase(std::next(moc.begin(), anIndex));
+}
+
+void Reference_Files::delCoordinate(int anIndex) {
+	coc.erase(std::next(coc.begin(), anIndex));
+}
+
+vector<string> Reference_Files::getCrashRooms() {
+	return crashRooms;
+}
