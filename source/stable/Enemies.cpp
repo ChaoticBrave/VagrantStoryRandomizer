295a5fa0bbfd184f447d52ecc5caf2c759b7a78a
ChaoticBrave <90638442+ChaoticBrave@users.noreply.github.com>
2023-06-11 18:20:34 +0200

    Update Enemies.cpp

--- source/stable/Enemies.cpp
+++ source/stable/Enemies.cpp
@@ -20,7 +20,7 @@ Enemies::Enemies() {
             
 }
 
-void Enemies::mapIterate(Reference_Files aRF, Add_Game& aGame, string aDecision) {
+void Enemies::mapIterate(Reference_Files aRF, Add_Game& aGame, string aDecision, std::mt19937 aGen) {
     list<string> z = aRF.getZones();
     list<string>::iterator zp = z.begin();
     vector<vector<string>> ml = aRF.getMapList();
@@ -55,8 +55,8 @@ void Enemies::mapIterate(Reference_Files aRF, Add_Game& aGame, string aDecision)
     std::stringstream sti;
     std::stringstream hquo_numer;
     std::stringstream hparnum;
-    std::random_device ran;
-    std::mt19937 gener(ran());
+    //std::random_device ran;
+    //std::mt19937 gener(ran());
     std::uniform_int_distribution<> ene_dist;
     std::uniform_int_distribution<> item_dist;
     std::uniform_int_distribution<> odds_dist;
@@ -130,30 +130,36 @@ void Enemies::mapIterate(Reference_Files aRF, Add_Game& aGame, string aDecision)
                         ene_at = acmap.tellg();
                         ene_loc = acmap.get();
                         ene_dist = std::uniform_int_distribution<>(0, (para_2 - 1));
-                        if ((cur_zone == "ZONE013.ZND" && (ene_loc == 0 || ene_loc == 1 || ene_loc == 2 || ene_loc == 3)) == false) {
-                            ene = ene_dist(gener);
-                            if (cur_zone == "ZONE013.ZND" && ene < 4) {
-                                while (cur_zone == "ZONE013.ZND" && ene < 4) {
-                                    ene_dist = std::uniform_int_distribution<>(0, (para_2 - 1));
-                                    ene = ene_dist(gener);
-                                }
+                        if ((cur_zone == "ZONE013.ZND" && (ene_loc == 0 || ene_loc == 1 || ene_loc == 2 || ene_loc == 3 || ene_loc == 4)) || (cur_zone == "ZONE015.ZND" && ene_loc == 0) == false) {
+                            ene = ene_dist(aGen);
+                            if (cur_zone == "ZONE013.ZND" && ene <= 4) {
+                                ene_dist = std::uniform_int_distribution<>(5, (para_2 - 1));
+                                ene = ene_dist(aGen);
+                            }
+                            if (cur_zone == "ZONE015.ZND" && ene == 0) {
+                                ene_dist = std::uniform_int_distribution<>(1, (para_2 - 1));
+                                ene = ene_dist(aGen);
                             }
                             if (cur_zone == "ZONE028.ZND" && ene == 15) {
                                 while (cur_zone == "ZONE028.ZND" && ene == 15) {
                                     ene_dist = std::uniform_int_distribution<>(0, (para_2 - 1));
-                                    ene = ene_dist(gener);
+                                    ene = ene_dist(aGen);
                                 }
                             }
                             if (cur_zone == "ZONE049.ZND" && ene == 6) {
-                                while (cur_zone == "ZONE028.ZND" && ene == 6) {
+                                while (cur_zone == "ZONE049.ZND" && ene == 6) {
                                     ene_dist = std::uniform_int_distribution<>(0, (para_2 - 1));
-                                    ene = ene_dist(gener);
+                                    ene = ene_dist(aGen);
                                 }
                             }
+                            //if (cur_zone == "ZONE032.ZND" && ene <= 2) {
+                              //  ene_dist = std::uniform_int_distribution<>(3, (para_2 - 1));
+                              //  ene = ene_dist(aGen);
+                            //}
                             if (ene == ene_loc) {
                                 while (ene == ene_loc) {
                                     ene_dist = std::uniform_int_distribution<>(0, (para_2 - 1));
-                                    ene = ene_dist(gener);
+                                    ene = ene_dist(aGen);
                                 }
                             }
                             ch_val = new char(ene);
@@ -165,7 +171,7 @@ void Enemies::mapIterate(Reference_Files aRF, Add_Game& aGame, string aDecision)
                     if (aDecision == "Y") {
                         for (int b = size - 9; b > ((size - (38 * round(quo_numer / 40))) - 40); b -= 40) {
                             item_dist = std::uniform_int_distribution<>(0, 511);
-                            ind_item = item_dist(gener);
+                            ind_item = item_dist(aGen);
                             if (ind_item > 255) {
                                 acmap.seekp(b + 1, ios::beg);
                                 ch_val = new char(1);
@@ -180,7 +186,7 @@ void Enemies::mapIterate(Reference_Files aRF, Add_Game& aGame, string aDecision)
                         }
                         for (int b = size - 5; b > ((size - (38 * round(quo_numer / 40))) - 40); b -= 40) {
                             odds_dist = std::uniform_int_distribution<>(1, 100);
-                            odds = odds_dist(gener);
+                            odds = odds_dist(aGen);
                             ch_val = new char(odds);
                             acmap.seekp(b, ios::beg);
                             acmap.write(ch_val, 1);
